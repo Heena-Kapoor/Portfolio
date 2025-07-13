8db155907cbec4a3c1847cae1fb547004f6aac72
Heena-Kapoor <hkkapoor257@gmail.com>
2025-07-13 10:42:46 +0530

    import error

--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import ExperienceCard from '../Cards/ExperienceCard';
+import ExperienceCard from '../cards/ExperienceCard';
 
 const Container = styled.section`
   display: flex;
